5341589e347b64c67bb3234d4fe2742bcfe87a5a
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-07-27 10:36:06 +0530

    corrections

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  experimental: {
+    serverActions: {},
+  },
 };
 
 export default nextConfig;
+
+
+
+
+
